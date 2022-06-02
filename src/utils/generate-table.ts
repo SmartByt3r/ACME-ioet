72dcfa3ef02b843bfae9b50d0ce4f0b865823fbd
Nicolas Cisneros <bryannicolas67@gmail.com>
2022-06-01 23:30:13 -0500

    fix: generateTable function was modifying the employees reference

--- src/utils/generate-table.ts
+++ src/utils/generate-table.ts
@@ -8,9 +8,9 @@ export type TableRow = { names: string; numberOfCoincidences: number };
  * @returns The table
  */
 export function generateTable(employees: Employee[]): TableRow[] {
-  type NewType = TableRow;
-
-  const iterateOverWorkDays = (row: NewType, firstEmployee: Employee) => {
+  const table: TableRow[] = [];
+  const _employees = [...employees];
+  const iterateOverWorkDays = (row: TableRow, firstEmployee: Employee) => {
     return (workDay: WorkDay) => {
       firstEmployee.workedSchedule.forEach((otherWorkDay) => {
         if (otherWorkDay.coincidesWith(workDay)) row.numberOfCoincidences++;
@@ -30,11 +30,9 @@ export function generateTable(employees: Employee[]): TableRow[] {
     };
   };
 
-  const table: TableRow[] = [];
-  while (employees.length > 0) {
-    const firstEmployee = employees.shift();
-    if (!firstEmployee) break;
-    employees.forEach(iterateOverEmployees(firstEmployee));
+  while (_employees.length > 0) {
+    const firstEmployee = _employees.shift();
+    _employees.forEach(iterateOverEmployees(firstEmployee!));
   }
   return table;
 }
@@ -44,7 +42,9 @@ export function generateTable(employees: Employee[]): TableRow[] {
  * @param table The table to print
  */
 export function printTable(table: TableRow[]) {
-  table.forEach((row) => {
-    console.log(`${row.names}: ${row.numberOfCoincidences}`);
-  });
+  let outStr = table.reduce((acc, val, index) => {
+    const entry = `${val.names}: ${val.numberOfCoincidences}`;
+    return acc.concat(index + 1 === table.length ? `${entry}` : `${entry}\n`);
+  }, "");
+  console.log(outStr);
 }
